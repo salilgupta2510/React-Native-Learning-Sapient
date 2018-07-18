153576932c20c1289ae7d912c7b187a693fccd6c
Gaurav Mehta <gaumehta1@publicisgroupe.net>
2018-07-18 14:29:02 +0530

    updated package.json

--- exercise-3-header-with-mobx/packages/shared/src/stores/test-support/test-root.store.ts
+++ exercise-3-header-with-mobx/packages/shared/src/stores/test-support/test-root.store.ts
@@ -1,8 +1,7 @@
 import { RouterState, RouterStore } from 'mobx-state-router';
-import { routes } from './routes';
 
 const notFound = new RouterState('notFound');
 
 export class RootStore {
-    routerStore = new RouterStore(this, routes, notFound);
+    //routerStore = new RouterStore(this,  notFound);
 }
